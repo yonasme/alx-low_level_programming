bb6de05810e01b2613f51e36700739c473fbd6fc
yonas <yonasg57@gmail.com>
2022-04-08 00:10:46 +0300

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Return:1
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+write(1, ch,59);
 return (1);
 }
